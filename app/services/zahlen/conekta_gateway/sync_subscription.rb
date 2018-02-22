25f7570954606205d44863ba317d12a08fe86a96
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-22 16:09:46 -0600

    Min fix for sync sub

--- app/services/zahlen/conekta_gateway/sync_subscription.rb
+++ app/services/zahlen/conekta_gateway/sync_subscription.rb
@@ -5,8 +5,8 @@ module Zahlen
         customer = Conekta::Customer.find(sub.gateway_customer_id)
         conekta_sub = customer.subscription
 
-        sub.current_period_start = Time.at(conekta_sub.billing_cycle_start)
-        sub.current_period_end   = Time.at(conekta_sub.billing_cycle_end)
+        sub.current_period_start = Time.at(conekta_sub.billing_cycle_start) if conekta_sub.try(:billing_cycle_start)
+        sub.current_period_end   = Time.at(conekta_sub.billing_cycle_end) if conekta_sub.try(:billing_cycle_end)
         sub.ended_at             = Time.at(conekta_sub.ended_at) if conekta_sub.try(:ended_at)
         sub.trial_start          = Time.at(conekta_sub.trial_start) if conekta_sub.try(:trial_start)
         sub.trial_end            = Time.at(conekta_sub.trial_end) if conekta_sub.try(:trial_end)
