379fd65ad91606a8b46838312f7839e89832c5c0
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-22 18:31:44 -0600

    Add missing comma

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Zahlen::Engine.routes.draw do
   get   '/subscription/:uuid/status', to: 'subscriptions#status',      as: :zahlen_subscription_status
   get   '/subscription/:uuid',        to: 'subscriptions#show',        as: :zahlen_subscription
-  post  '/change_plan/:uuid'          to: 'subscriptions#change_plan', as: :zahlen_change_subscription_plan
+  post  '/change_plan/:uuid',         to: 'subscriptions#change_plan', as: :zahlen_change_subscription_plan
 
   mount ConektaEvent::Engine => '/webooks/'
 end
