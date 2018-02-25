a4043be740b0b80d24e97d05c283350bf564531b
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-24 23:06:42 -0600

    Fix to update subscription card data on subscription

--- app/services/zahlen/conekta_gateway/change_subscription_card.rb
+++ app/services/zahlen/conekta_gateway/change_subscription_card.rb
@@ -6,7 +6,14 @@ module Zahlen
           customer = Zahlen::ConektaGateway::Customer.find_or_create(subscription)
           source   = customer.create_payment_source(type: "card", token_id: card_token)
           customer.subscription.update(card: source.id)
-          subscription.sync_gateway
+          prefix_exp_year = Time.zone.now.year.to_s[0..1]
+          subscription.update_attributes(
+            card_holdername: card.name,
+            card_last4: card.last4,
+            card_brand: card.brand,
+            card_bin: card.bin,
+            card_expiration: Date.new("#{prefix_exp_year}#{card.exp_year}".to_i, card.exp_month.to_i, 1)
+          )
         rescue Conekta::Error => error
           errors = []
           for error_detail in error.details do
