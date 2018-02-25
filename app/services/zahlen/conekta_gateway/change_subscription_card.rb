d10d5d85c6bc113eb65ad27fb8387c1d92971b62
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-24 23:24:11 -0600

    Remove space

--- app/services/zahlen/conekta_gateway/change_subscription_card.rb
+++ app/services/zahlen/conekta_gateway/change_subscription_card.rb
@@ -4,7 +4,7 @@ module Zahlen
       def self.call(subscription, card_token)
         begin
           customer = Zahlen::ConektaGateway::Customer.find_or_create(subscription)
-          card   = customer.create_payment_source(type: "card", token_id: card_token)
+          card = customer.create_payment_source(type: "card", token_id: card_token)
           customer.subscription.update(card: card.id)
           prefix_exp_year = Time.zone.now.year.to_s[0..1]
           subscription.update_attributes(
