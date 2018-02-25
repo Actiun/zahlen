2e9087afefa9a4afa65533e4e11446e421dce6fd
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-24 18:52:33 -0600

    Add missing end

--- app/services/zahlen/conekta_gateway/change_subscription_card.rb
+++ app/services/zahlen/conekta_gateway/change_subscription_card.rb
@@ -2,8 +2,9 @@ module Zahlen
   module ConektaGateway
     class ChangeSubscriptionCard
       def self.call(subscription, card_token)
-        customer = Zahlen::ConektaGateway::Customer.find_or_create(subscription)
-        customer.subscription.update(card: card_token)
+        begin
+          customer = Zahlen::ConektaGateway::Customer.find_or_create(subscription)
+          customer.subscription.update(card: card_token)
         rescue Conekta::Error => error
           errors = []
           for error_detail in error.details do
