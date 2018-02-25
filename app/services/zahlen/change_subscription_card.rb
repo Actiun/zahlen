d3035ccff1d25b50785e05c4e8a2fe5ddffde289
Jorge Najera <jorge.najera.t@gmail.com>
2018-02-25 09:12:07 -0600

    Remove white spaces

--- app/services/zahlen/change_subscription_card.rb
+++ app/services/zahlen/change_subscription_card.rb
@@ -2,9 +2,9 @@ module Zahlen
   class ChangeSubscriptionCard
     def self.call(subscription, card_token)
       case Zahlen.gateway
-       when 'conekta'
-         Zahlen::ConektaGateway::ChangeSubscriptionCard.call(subscription, card_token)
-       end
+      when 'conekta'
+        Zahlen::ConektaGateway::ChangeSubscriptionCard.call(subscription, card_token)
+      end
     end
   end
 end
